ced72d653d6f6c99967c9afa7b5d7f909d1be275
Jessica Anne <113569098+jessica-anne21@users.noreply.github.com>
2024-10-22 21:31:24 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="text-center py-3" style="background-color: #f1f1f1;">
+    <p>Created by: <br>
+        1972035 - Jonathan Christandy Darmawan<br>
+        2072031 - Devin Aditya Putra Pratama<br>
+        2272031 - Jessica Anne<br>
+        2272048 - Selin Velday Papilaya
+    </p>
+</footer>
