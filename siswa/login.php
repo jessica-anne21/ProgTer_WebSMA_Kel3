4e022d029055207cc3831dc3e24ed6bdcedd6948
Jessica Anne <113569098+jessica-anne21@users.noreply.github.com>
2024-10-25 15:28:53 +0700

    Update login.php

--- siswa/login.php
+++ siswa/login.php
@@ -1,21 +1,19 @@
 <?php
 session_start();
-include('../includes/db.php'); // Make sure this points to the correct database connection file
+include('../includes/db.php'); 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    // Query to get the user with the role 'guru' and the entered username
-    $query = "SELECT * FROM users WHERE username='$username' AND role='guru' LIMIT 1";
+    $query = "SELECT * FROM users WHERE username='$username' AND role='siswa' LIMIT 1";
     $result = mysqli_query($conn, $query);
     $user = mysqli_fetch_assoc($result);
 
     if ($user && password_verify($password, $user['password'])) {
-        // If the user is found and the password is correct
         $_SESSION['user'] = $user['username'];
-        $_SESSION['role'] = $user['role']; // Set session role as 'guru'
-        header('Location: ../guru/guru_dashboard.php'); // Redirect to Guru's dashboard
+        $_SESSION['role'] = $user['role']; 
+        header('Location: ../siswa/siswa.php'); 
         exit();
     } else {
         $error = "Username atau password salah!";
