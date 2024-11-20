a4c13b6164853500f951324973b84597b9a307ef
Jessica Anne <113569098+jessica-anne21@users.noreply.github.com>
2024-11-21 00:57:24 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,7 @@
 <footer class="text-center py-4" style="background-color: #002147; color: white;">
     <div class="container">
         <h5>Created by:</h5>
-        <ul class="list-unstyled">
-            <li>1972035 - Jonathan Christandy Darmawan</li>
-            <li>2072031 - Devin Aditya Putra Pratama</li>
-            <li>2272031 - Jessica Anne</li>
-            <li>2272048 - Selin Velday Papilaya</li>
-        </ul>
+        <p>1972035 - Jonathan Christandy Darmawan | 2072031 - Devin Aditya Putra Pratama | 2272031 - Jessica Anne | 2272048 - Selin Velday Papilaya</p>
         <p class="mt-3">&copy; 2024 All Rights Reserved. - Pemrograman Terapan</p>
     </div>
 </footer>
