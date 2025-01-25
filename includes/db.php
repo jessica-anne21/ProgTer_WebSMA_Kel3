888ad80545c753be4aa07ad95965bca56dad527c
Jessica Anne <113569098+jessica-anne21@users.noreply.github.com>
2025-01-25 17:08:09 +0700

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,13 @@
 <?php
 $host = 'localhost';
-$db_name = 'school_1';
-$username = 'root'; 
-$password = ''; 
+$db_name = 'school_2';
+$username = 'root';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_AUTOCOMMIT, true); 
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
